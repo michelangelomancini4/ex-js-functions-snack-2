6bc2e8a2adcc7de2a622f8cca4f63c16b5decafc
michelangelomancini <michelangelomancini8@gmail.com>
2025-04-30 10:36:14 +0200

    Snack ex-js 02 done

---
+++ 02_quadrato/script.js
@@ -0,0 +1,14 @@
+// Crea una arrow function che calcola il quadrato di un numero.
+
+let numquadro = (num1) => { return (num1 * num1) }
+
+console.log(numquadro(4));
+
+
+// Definisci una funzione chiamata quadrato che accetta un numero e restituisce il suo quadrato in una sola riga.
+
+function quadrato(num) { return num * num; }
+
+console.log(quadrato(6));
+
+
