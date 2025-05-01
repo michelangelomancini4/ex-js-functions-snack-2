3b2605ad6856b98d5bcac61bb6a037a7d69a3c4f
michelangelomancini <michelangelomancini8@gmail.com>
2025-05-01 10:20:59 +0200

    Snack ex-js 09(bonus) done

---
+++ 09_sequenzaOperazioni/script.js
@@ -0,0 +1,23 @@
+// Creare una funzione che esegue una sequenza di operazioni con ritardi
+
+// Scrivi una funzione sequenzaOperazioni che accetta un array di operazioni (funzioni) e un tempo di intervallo.
+// Ogni operazione deve essere eseguita in sequenza con un ritardo uguale al tempo di intervallo.
+
+function sequenzaOperazioni(arrayFunzioni, time) {
+    return arrayFunzioni.forEach((funzione, index) => {
+        setTimeout(() => {
+            funzione()
+        }, time * index)
+    });
+
+
+}
+
+sequenzaOperazioni([
+    () => console.log("Operazione 1"),
+    () => console.log("Operazione 2"),
+    () => console.log("Operazione 3")
+], 2000);
+
+
+
